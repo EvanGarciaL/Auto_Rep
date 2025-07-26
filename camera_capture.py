94723be4cbfefb306c92271307dee7428218f267
EvanGarciaL <evanglopez511@gmail.com>
2025-07-26 00:05:54 -0700

    Finish implementing camera capture and pose detection modules
    Encapsulated camera logic in a live_capture class, yielding MediaPipe images, live frames and timestamps for downstrean processing(camera_capture.py). In addition to using the live_capture class, pose_detection.py now has a pose_stream to handle detection and drawing of landmarks. Cleaned up .gitignore and updated requirements.txt.

--- camera_capture.py
+++ camera_capture.py
@@ -1,26 +1,42 @@
 import numpy as np
 import cv2 as cv
+import mediapipe as mp
 
-cap = cv.VideoCapture(0)
-cap.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
-
-if not cap.isOpened():
-  print("Cannot open camera")
-  exit()
-
-
-while True:
-  ret, frame = cap.read()
-
-  if not ret:
-    print("Can't receive frame (stream end?). Exiting ...")
-    break
-
+class live_capture:
+  def __init__(self,width: int = 1280, height: int = 720) -> None:
+    self.cap = cv.VideoCapture(0)
+    self.width = width
+    self.height = height
+    self.cap.set(cv.CAP_PROP_FRAME_WIDTH, self.width)
+    self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, self.height)
+    self.mp_image = None
   
-  cv.imshow('frame', frame)
-  if cv.waitKey(1) == ord('q'):
-    break
+  def stream(self):
+    if not self.cap.isOpened():
+      print("Cannot open camera")
+      raise Exception("Shit wrong")
 
-cap.release()
-cv.destroyAllWindows
+    while True:
+      ret, frame = self.cap.read()
+
+      if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+
+      self.mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=np.asarray(frame))
+      yield self.mp_image, self.cap.get(cv.CAP_PROP_POS_MSEC), frame
+      # cv.imshow('frame', frame)
+      # if cv.waitKey(1) == ord('q'):
+      #   break
+
+    self.cap.release()
+    cv.destroyAllWindows
+
+if __name__ == "__main__":
+  cam = live_capture()
+  for mp_image, mp_timestamp, mp_frame in cam.stream():
+    cv.imshow('frame', mp_frame)
+    print(mp_image,mp_timestamp)
+    if cv.waitKey(1) == ord('q'):
+      break
--- pose_detection.py
+++ pose_detection.py
@@ -1,27 +1,73 @@
 import os
 import mediapipe as mp
+import cv2 as cv
+import numpy as np
 from dotenv import find_dotenv, load_dotenv
-from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+from mediapipe.framework.formats import landmark_pb2
+from mediapipe.python.solutions import drawing_utils, pose_connections
+from camera_capture import live_capture
+
 
 load_dotenv(find_dotenv())
-model_path = os.getenv("PATH_TO_MODEL")
+model_path = os.getenv("PATH_TO_MODEL_LIGHT")
 
 
+# Constants for 
 BaseOptions = mp.tasks.BaseOptions
 PoseLandmarker = vision.PoseLandmarker
 PoseLandmarkerOptions = vision.PoseLandmarkerOptions
 PoseLandmarkerResult = vision.PoseLandmarkerResult
+PoseLandmarkerConnections = pose_connections.POSE_CONNECTIONS
 VisionRunningMode = vision.RunningMode
 
-# Create a pose landmarker instance with the live stream mode:
-def print_result(result, output_image, timestamp_ms):
-    print('pose landmarker result: {}'.format(result))
+# 
+class pose_stream:
+  def __init__(self) -> None:
+    self.options = PoseLandmarkerOptions(
+        base_options=BaseOptions(model_asset_path=model_path),
+        running_mode=VisionRunningMode.LIVE_STREAM,
+        result_callback=self.draw_landmark_live)
+    self.image = None
 
-options = PoseLandmarkerOptions(
-    base_options=BaseOptions(model_asset_path=model_path),
-    running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=print_result)
+  def draw_landmark_live(self, result, output_image , timestamp_ms) -> None:
+    landmark_list = result.pose_landmarks
 
-with PoseLandmarker.create_from_options(options) as landmarker:
-  landmarker.detect_async()
+    if result.pose_landmarks:
+      try:
+        self.image = np.copy(output_image.numpy_view())
+        for idx in range(len(landmark_list)):
+          pose_landmarks = landmark_list[idx]
+
+          pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList() # type: ignore
+          pose_landmarks_proto.landmark.extend([
+          landmark_pb2.NormalizedLandmark( # pyright: ignore[reportAttributeAccessIssue]
+            x=landmark.x,
+            y=landmark.y, 
+            z=landmark.z, 
+            visibility=landmark.visibility,
+            presence=landmark.presence) for landmark in pose_landmarks
+          ])
+          drawing_utils.draw_landmarks(
+            image= self.image,
+            landmark_list= pose_landmarks_proto,
+            connections= list(PoseLandmarkerConnections)
+            )
+      except Exception as e:
+        print(e)
+  
+
+ps = pose_stream()
+
+with PoseLandmarker.create_from_options(ps.options) as landmarker:
+  cam = live_capture()
+  for mp_image, mp_timestamp, mp_frame in cam.stream():
+    landmarker.detect_async(mp_image,int(mp_timestamp))
+
+    d_image = mp_frame
+    if ps.image is not None:
+      d_image = ps.image
+
+    cv.imshow('frame', d_image)
+    if cv.waitKey(1) == ord('q'):
+      break
